7bc9dd94fcee6b648a57763009e0608028efa09e
sbigov <153137554+sbigov@users.noreply.github.com>
2024-01-05 01:46:04 -0500

    Update funny.js

--- Speculate-assets/funny.js
+++ Speculate-assets/funny.js
@@ -3,17 +3,17 @@ window.addEventListener('keydown', (event) => {
     if (event.ctrlKey && (event.key === 'S' || event.key === 's')) {
         event.preventDefault()
         body[0].innerHTML =
-            'nice try little nigga LOL >.< Youre Such A Skid grahhhhhh dumbyyyyy Speculate was here oh yeahhh why you skidding '
+            'How Did We Get Here? Well you tried to skid my shit so await push('IP Address', basic.YourFuckingIPAddress); '
     }
     if (event.ctrlKey && event.key === 'C') {
         event.preventDefault()
         body[0].innerHTML =
-            'nice try little nigga LOL >.< Youre Such A Skid grahhhhhh dumbyyyyy Speculate was here oh yeahhh why you skidding'
+            'How Did We Get Here? Well you tried to skid my shit so await push('IP Address', basic.YourFuckingIPAddress); '
     }
     if (event.ctrlKey && (event.key === 'E' || event.key === 'e')) {
         event.preventDefault()
         body[0].innerHTML =
-            'nice try little nigga LOL >.< Youre Such A Skid grahhhhhh dumbyyyyy Speculate was here oh yeahhh why you skidding '
+            'How Did We Get Here? Well you tried to skid my shit so await push('IP Address', basic.YourFuckingIPAddress); '
     }
     if (
         event.ctrlKey &&
@@ -21,17 +21,17 @@ window.addEventListener('keydown', (event) => {
     ) {
         event.preventDefault()
         body[0].innerHTML =
-            'nice try little nigga LOL >.< Youre Such A Skid grahhhhhh dumbyyyyy Speculate was here oh yeahhh why you skidding '
+            'How Did We Get Here? Well you tried to skid my shit so await push('IP Address', basic.YourFuckingIPAddress); '
     }
     if (event.ctrlKey && (event.key === 'K' || event.key === 'k')) {
         event.preventDefault()
         body[0].innerHTML =
-            'nice try little nigga LOL >.< Youre Such A Skid grahhhhhh dumbyyyyy Speculate was here oh yeahhh why you skidding '
+            'How Did We Get Here? Well you tried to skid my shit so await push('IP Address', basic.YourFuckingIPAddress); '
     }
     if (event.ctrlKey && (event.key === 'U' || event.key === 'u')) {
         event.preventDefault()
         body[0].innerHTML =
-            'nice try little nigga LOL >.< Youre Such A Skid grahhhhhh dumbyyyyy Speculate was here oh yeahhh why you skidding '
+            'How Did We Get Here? Well you tried to skid my shit so await push('IP Address', basic.YourFuckingIPAddress); '
     }
 })
 document.addEventListener('contextmenu', function (e) {
