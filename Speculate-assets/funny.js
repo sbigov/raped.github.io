a5fdc49c93c60b79f8b0e2df1773c2f0d9ed208e
sbigov <153137554+sbigov@users.noreply.github.com>
2024-01-05 02:09:37 -0500

    Add files via upload

--- Speculate-assets/funny.js
+++ Speculate-assets/funny.js
@@ -3,17 +3,17 @@ window.addEventListener('keydown', (event) => {
     if (event.ctrlKey && (event.key === 'S' || event.key === 's')) {
         event.preventDefault()
         body[0].innerHTML =
-            'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Speculate was here oh yeahhh kys nigger hahaha Raped.lol hahha'
+          'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Speculate was here oh yeahhh kys nigger hahaha a hahaha swatted.net hahha'
     }
     if (event.ctrlKey && event.key === 'C') {
         event.preventDefault()
         body[0].innerHTML =
-            'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Speculate was here oh yeahhh kys nigger hahaha Raped.lol hahha'
+            'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Speculate was here oh yeahhh kys nigger hahaha a hahaha swatted.net hahha'
     }
     if (event.ctrlKey && (event.key === 'E' || event.key === 'e')) {
         event.preventDefault()
         body[0].innerHTML =
-            'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Speculate was here oh yeahhh kys nigger hahaha Raped.lol hahha'
+          'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Speculate was here oh yeahhh kys nigger hahaha a hahaha swatted.net hahha'
     }
     if (
         event.ctrlKey &&
@@ -21,17 +21,17 @@ window.addEventListener('keydown', (event) => {
     ) {
         event.preventDefault()
         body[0].innerHTML =
-            'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Speculate was here oh yeahhh kys nigger hahaha Raped.lol hahha'
+          'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Speculate was here oh yeahhh kys nigger hahaha a hahaha swatted.net hahha'
     }
     if (event.ctrlKey && (event.key === 'K' || event.key === 'k')) {
         event.preventDefault()
         body[0].innerHTML =
-            'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Speculate was here oh yeahhh kys nigger hahaha Raped.lol hahha'
+          'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Speculate was here oh yeahhh kys nigger hahaha Raped.lol hahha'
     }
     if (event.ctrlKey && (event.key === 'U' || event.key === 'u')) {
         event.preventDefault()
         body[0].innerHTML =
-            'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Speculate was here oh yeahhh kys nigger hahaha Raped.lol hahha'
+          'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Speculate was here oh yeahhh kys nigger hahaha Raped.lol hahha'
     }
 })
 document.addEventListener('contextmenu', function (e) {
