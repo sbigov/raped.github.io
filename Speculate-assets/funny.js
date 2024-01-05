d242177b23c95e5147fa8ad706e222d830723370
sbigov <153137554+sbigov@users.noreply.github.com>
2024-01-04 22:39:45 -0500

    Add files via upload

---
+++ Speculate-assets/funny.js
@@ -0,0 +1,39 @@
+const body = document.getElementsByTagName('body')
+window.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && (event.key === 'S' || event.key === 's')) {
+        event.preventDefault()
+        body[0].innerHTML =
+            'nice try little nigga LOL >.< Youre Such A Skid grahhhhhh dumbyyyyy Speculate was here oh yeahhh why you skidding '
+    }
+    if (event.ctrlKey && event.key === 'C') {
+        event.preventDefault()
+        body[0].innerHTML =
+            'nice try little nigga LOL >.< Youre Such A Skid grahhhhhh dumbyyyyy Speculate was here oh yeahhh .gg/streamables hahaha swatted.net hahha'
+    }
+    if (event.ctrlKey && (event.key === 'E' || event.key === 'e')) {
+        event.preventDefault()
+        body[0].innerHTML =
+            'nice try little nigga LOL >.< Youre Such A Skid grahhhhhh dumbyyyyy Speculate was here oh yeahhh why you skidding '
+    }
+    if (
+        event.ctrlKey &&
+        (event.key === 'I' || event.key === 'i' || event.key === 'Ä\xB1')
+    ) {
+        event.preventDefault()
+        body[0].innerHTML =
+            'nice try little nigga LOL >.< Youre Such A Skid grahhhhhh dumbyyyyy Speculate was here oh yeahhh why you skidding '
+    }
+    if (event.ctrlKey && (event.key === 'K' || event.key === 'k')) {
+        event.preventDefault()
+        body[0].innerHTML =
+            'nice try little nigga LOL >.< Youre Such A Skid grahhhhhh dumbyyyyy Speculate was here oh yeahhh why you skidding '
+    }
+    if (event.ctrlKey && (event.key === 'U' || event.key === 'u')) {
+        event.preventDefault()
+        body[0].innerHTML =
+            'nice try little nigga LOL >.< Youre Such A Skid grahhhhhh dumbyyyyy Speculate was here oh yeahhh why you skidding '
+    }
+})
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault()
+})
---
+++ Speculate-assets/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'Ran You','LIL SKID','connecting :O','Fuck Pedos','Sim Swapped :O','Yall Talk Too Much Shit','https://Raped.lol :D'],
+  typeSpeed : 35,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})
